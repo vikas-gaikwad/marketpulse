40d2fa1d821f20dd3aeb6ce7fbb0e0dd23aea3c8
vikas-gaikwad <vikasgaikwad201415@gmail.com>
2026-09-15 19:16:26 +0530

    chore: initialize MarketPulse project

---
+++ src/test/java/com/marketpulse/MarketpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
