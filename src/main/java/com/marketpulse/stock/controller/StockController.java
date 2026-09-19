0b0bdc16d52a61c2f772a8907b70cafcecb8db10
vikas-gaikwad <vikasgaikwad201415@gmail.com>
2026-09-19 08:57:22 +0530

    V1.8.0 - Add pagination and sorting

--- src/main/java/com/marketpulse/stock/controller/StockController.java
+++ src/main/java/com/marketpulse/stock/controller/StockController.java
@@ -1,5 +1,6 @@
 package com.marketpulse.stock.controller;
 
+import com.marketpulse.exception.ErrorResponse;
 import com.marketpulse.stock.dto.StockRequest;
 import com.marketpulse.stock.dto.StockResponse;
 import com.marketpulse.stock.dto.StockUpdateRequest;
@@ -8,12 +9,14 @@ import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 //It tells Spring that this class handles REST requests and its methods return data for the HTTP response.
@@ -25,32 +28,60 @@ public class StockController {
         this.stockService = stockService;
     }
 
+    private static final Set<String> ALLOWED_SORT_FIELDS =
+            Set.of("symbol", "companyName", "exchange", "sector");
+
     @GetMapping("/{symbol}")
-    public ResponseEntity<StockResponse> getStockBySymbol(@PathVariable String symbol){
-        StockResponse stockResponse =this.stockService.getStockBySymbol(symbol);
+    public ResponseEntity<StockResponse> getStockBySymbol(@PathVariable String symbol) {
+        StockResponse stockResponse = this.stockService.getStockBySymbol(symbol);
         return ResponseEntity.ok(stockResponse);
     }
+
     @PostMapping
     public ResponseEntity<StockResponse> createStock(
             @RequestBody @Valid StockRequest stockRequest) {
         // We will call the service here
-        StockResponse stockResponse= this.stockService.createStock(stockRequest);
-        return  ResponseEntity.status(HttpStatus.CREATED).body(stockResponse);
+        StockResponse stockResponse = this.stockService.createStock(stockRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(stockResponse);
     }
+
     @GetMapping
-    public ResponseEntity<Page<StockResponse>> getAllStocks(@PageableDefault(size = 10,page = 0) Pageable pageable) {
-        // call service
-        if (pageable.getPageSize()>50){
-            PageRequest.of(pageable.getPageNumber(),50,pageable.getSort());
+    public ResponseEntity<?> getAllStocks(
+            @PageableDefault(size = 10, page = 0) Pageable pageable) {
+
+        for (Sort.Order order : pageable.getSort()) {
+
+            if (!ALLOWED_SORT_FIELDS.contains(order.getProperty())) {
+
+                ErrorResponse errorResponse = new ErrorResponse(
+                        HttpStatus.BAD_REQUEST.value(),
+                        "Invalid sort field: " + order.getProperty()
+                );
+
+                return ResponseEntity.badRequest().body(errorResponse);
+            }
         }
-        Page<StockResponse> stockResponse=this.stockService.getAllStocks(pageable);
+
+        if (pageable.getPageSize() > 50) {
+            pageable = PageRequest.of(
+                    pageable.getPageNumber(),
+                    50,
+                    pageable.getSort()
+            );
+        }
+
+        Page<StockResponse> stockResponse =
+                stockService.getAllStocks(pageable);
+
         return ResponseEntity.ok(stockResponse);
     }
+
     @PutMapping("/{symbol}")
     public ResponseEntity<StockResponse> updateStock(@PathVariable String symbol, @RequestBody @Valid StockUpdateRequest stockUpdateRequest) {
         StockResponse stockResponse = stockService.updateStock(symbol, stockUpdateRequest);
         return ResponseEntity.ok(stockResponse);
     }
+
     @DeleteMapping("/{symbol}")
     public ResponseEntity<Void> deleteStock(@PathVariable String symbol) {
         // call service
